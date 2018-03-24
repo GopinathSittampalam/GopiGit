ac34bf7884057cb4a2621c4b4f5decb53ba9220f
S.Gopinath <sittampalamgopinath@gmail.com>
2018-03-24 22:17:26 +0530

    Db connection parameter changed

--- conf/config.php
+++ conf/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SEVER', getenv('MYSQL_SERVICE_HOST'));
-define('DB_USER', getenv('databaseuser'));
-define('DB_PASS', getenv('databasepassword'));
-define('DB_DATABASE', getenv('databasename'));
+define('DB_USER', getenv('MYSQL_USER'));
+define('DB_PASS', getenv('MYSQL_PASSWORD'));
+define('DB_DATABASE', getenv('MYSQL_DATABASE'));
 
 // Create connection
 $conn = mysqli_connect(DB_SEVER, DB_USER, DB_PASS,DB_DATABASE);
