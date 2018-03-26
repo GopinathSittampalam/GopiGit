66b47e9c6d45772461248441f0354e27cbb9d3c1
GopinathSittampalam <sittampalamgopinath@gmail.com>
2018-03-26 19:32:28 +0530

    students detail page added

--- students.php
+++ students.php
@@ -7,12 +7,12 @@
   <h1>Sample Data</h1>
 <table border="1">
   <tr>
-    <td>FID</td>
-    <td>student ID</td>
+    <td>Reg#</td>
+    <td>student Name</td>
   </tr>
 <?php
 require_once('conf/config.php');
-$query="select * from  Finder_ID_DB";
+$query="select * from  Student_Details";
     $result=mysqli_query($conn,$query);
     if (!$result) {
       die("could not perform query".mysqli_error($conn));
@@ -21,19 +21,14 @@ $query="select * from  Finder_ID_DB";
  
           while ($rows=mysqli_fetch_assoc($result)) {
           	echo "<tr>";
-            	echo "<td>".$rows['Finger_id']."</td>";
             	echo "<td>".$rows['Student_reg']."</td>";
+            	echo "<td>".$rows['Name']."</td>";
     		    echo "</tr>";
           
       }
   }
   mysqli_close($conn);
 ?>
-<tr>
-    <td colspan="2">
-      <a href="students.php">students details</a>
-    </td>
-  </tr>
 </table>
 </body>
 </html>
